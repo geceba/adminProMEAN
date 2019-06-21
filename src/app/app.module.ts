bfe824a3f4897d8e786fe1dfe84217c63d16f202
Gerardo Cetzal <gerardoce23@gmail.com>
2019-06-21 10:38:37 -0500

    template

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,26 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './login/login.component';
+import { NopagefoundComponent } from './shared/nopagefound/nopagefound.component';
+import { DashboardComponent } from './pages/dashboard/dashboard.component';
+import { ProgressComponent } from './pages/progress/progress.component';
+import { Graficas1Component } from './pages/graficas1/graficas1.component';
+import { HeaderComponent } from './shared/header/header.component';
+import { SidebarComponent } from './shared/sidebar/sidebar.component';
+import { BreadcrumbsComponent } from './shared/breadcrumbs/breadcrumbs.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    NopagefoundComponent,
+    DashboardComponent,
+    ProgressComponent,
+    Graficas1Component,
+    HeaderComponent,
+    SidebarComponent,
+    BreadcrumbsComponent
   ],
   imports: [
     BrowserModule,
