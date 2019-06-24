9c801665ccea1d3e81b915b591fe728022b169db
Gerardo Cetzal <gerardoce23@gmail.com>
2019-06-24 11:06:10 -0500

    plugins externos para importalo en angular

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,7 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+
+declare function init_plugins();
 
 @Component({
   selector: 'app-login',
@@ -7,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class LoginComponent implements OnInit {
 
-  constructor() { }
+  constructor( public _router: Router) { }
 
   ngOnInit() {
+    init_plugins();
+  }
+
+  ingresar() {
+    this._router.navigate([ '/dashboard'] );
   }
 
 }
